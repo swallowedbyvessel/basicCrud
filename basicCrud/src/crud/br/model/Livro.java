5a6fd0162ac76469943a14df92be6e8cc640036d
swallowedbyvessel <pudimdeleite001@gmail.com>
2024-01-19 19:32:35 -0300

    fixing errors and upoload the right repositories

---
+++ basicCrud/src/crud/br/model/Livro.java
@@ -0,0 +1,8 @@
+package crud.br.model;
+
+public class Livro {
+	String nome;
+	String descricao;
+	double valor;
+	String isbn;
+}
---
+++ basicCrud/src/crud/br/view/Crud.java
@@ -0,0 +1,9 @@
+package crud.br.view;
+
+public class Crud {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
